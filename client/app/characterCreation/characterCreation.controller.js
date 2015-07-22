11fcb033eb0a87d56e0fbf9822c26027ae5a6d08
Chris Sakai <cs.chris.sakai@gmail.com>
2015-07-22 19:50:21 -0400

    Start work on point selection

--- client/app/characterCreation/characterCreation.controller.js
+++ client/app/characterCreation/characterCreation.controller.js
@@ -2,13 +2,32 @@
 
 angular.module('dndApp')
   .controller('CharacterCreationCtrl', function ($scope) {
+    $scope.pointsRemaining = 27;
     $scope.abilities = [
-        {name: 'Strength', nameAbbr: 'Str', score: 10},
-        {name: 'Dexterity', nameAbbr: 'Dex', score: 10},
-        {name: 'Constitution', nameAbbr: 'Con', score: 10},
-        {name: 'Intelligence', nameAbbr: 'Int', score: 10},
-        {name: 'Wisdom', nameAbbr: 'Wis', score: 10},
-        {name: 'Charisma', nameAbbr: 'Cha', score: 10},
+      {name: 'Strength', nameAbbr: 'Str', points: 8},
+      {name: 'Dexterity', nameAbbr: 'Dex', points: 8},
+      {name: 'Constitution', nameAbbr: 'Con', points: 8},
+      {name: 'Intelligence', nameAbbr: 'Int', points: 8},
+      {name: 'Wisdom', nameAbbr: 'Wis', points: 8},
+      {name: 'Charisma', nameAbbr: 'Cha', points: 8},
+    ];
+    $scope.standardPoints = [
+      {points: 8, cost:0},
+      {points: 9, cost:1},
+      {points: 10, cost:2},
+      {points: 11, cost:3},
+      {points: 12, cost:4},
+      {points: 13, cost:5},
+      {points: 14, cost:7},
+      {points: 15, cost:9}
     ];
 
-  });
+    $scope.constructSelects = function () {
+      if ($scope.pointsRemaining >= 9) {
+        angular.forEach($scope.abilities, function (ability) {
+          ability.selectable = _.range(8, 16)
+        });
+      }
+    };
+    $scope.constructSelects();
+});

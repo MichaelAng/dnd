8716c4a68c0aada116d1aa0800f306f00e4038ba
Chris Sakai <cs.chris.sakai@gmail.com>
2015-07-22 20:33:33 -0400

    Create getCost and start of point Recalculation ng-change function. Tests for getCost.

--- client/app/characterCreation/characterCreation.controller.spec.js
+++ client/app/characterCreation/characterCreation.controller.spec.js
@@ -15,7 +15,44 @@ describe('Controller: CharacterCreationCtrl', function () {
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  it('should return 0 when ability points value is 8', function () {
+    var abilityPointValue = 8;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(0);
+  });
+  it('should return 1 when ability points value is 9', function () {
+    var abilityPointValue = 9;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(1);
+  });
+  it('should return 2 when ability points value is 10', function () {
+    var abilityPointValue = 10;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(2);
+  });
+  it('should return 3 when ability points value is 11', function () {
+    var abilityPointValue = 11;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(3);
+  });
+  it('should return 4 when ability points value is 12', function () {
+    var abilityPointValue = 12;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(4);
+  });
+      it('should return 5 when ability points value is 13', function () {
+    var abilityPointValue = 13;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(5);
+  });
+  it('should return 7 when ability points value is 14', function () {
+    var abilityPointValue = 14;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(7);
+  });
+  it('should return 9 when ability points value is 15', function () {
+    var abilityPointValue = 15;
+    var result = scope.getCost(abilityPointValue);
+    expect(result).toEqual(9);
   });
 });

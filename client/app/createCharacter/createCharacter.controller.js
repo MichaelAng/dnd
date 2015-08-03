afe71798a6638b72958df8117a5b77659730ebad
Michael Ang <angmerica@gmail.com>
2015-08-03 19:15:45 -0400

    sort and remove old artifact

--- client/app/createCharacter/createCharacter.controller.js
+++ client/app/createCharacter/createCharacter.controller.js
@@ -4,16 +4,11 @@ angular.module('dndApp')
   .controller('CreateCharacterCtrl', ['CreateCharacter', function (CreateCharacter) {
     var vm = this;
 
+    vm.characterClasses = CreateCharacter.characterClasses;
     vm.characterName = '';
     vm.races = CreateCharacter.races;
     vm.selectedCharacterClass = null;
     vm.selectedRace = null;
-
-    vm.characterClasses = [
-      {id: 1, characterClass: 'Cleric',  description: 'a normal guy'},
-      {id: 2, characterClass: 'Rogue',  description: 'pointy ears'},
-      {id: 3, characterClass: 'Wizard',  description: 'Roughy and tough'}
-    ];
 
     CreateCharacter.getRaces().then(function () {
       vm.races = CreateCharacter.races;

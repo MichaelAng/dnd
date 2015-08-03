b344ccc0e01b0dd9e11cb38ba34f9f79cb7a4f50
Michael Ang <angmerica@gmail.com>
2015-08-02 22:22:56 -0400

    removed static views and used ng-include

--- client/app/createCharacter/createCharacter.controller.js
+++ client/app/createCharacter/createCharacter.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('dndApp')
-  .controller('CreateCharacterCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('CreateCharacterCtrl', function () {
+    var vm = this;
+
+    vm.characterName = '';
+
+    vm.races = [
+        {id: 1, race: 'Human',  description: 'a normal guy'},
+        {id: 2, race: 'Elf',  description: 'pointy ears'},
+        {id: 3, race: 'Dwarf',  description: 'Roughy and tough'}
+    ];
+
+    vm.characterClasses = [
+        {id: 1, characterClass: 'Cleric',  description: 'a normal guy'},
+        {id: 2, characterClass: 'Rogue',  description: 'pointy ears'},
+        {id: 3, characterClass: 'Wizard',  description: 'Roughy and tough'}
+    ];
   });
--- client/app/createCharacter/createCharacter.js
+++ client/app/createCharacter/createCharacter.js
@@ -6,21 +6,11 @@ angular.module('dndApp')
       .state('createCharacter', {
         url: '/create-character',
         templateUrl: 'app/createCharacter/createCharacter.view.html',
-        controller: 'CreateCharacterCtrl',
         abstract: true
       })
       .state('createCharacter.raceClassPicker', {
         url: '/race-class-picker',
-        views: {
-          '': {
-            templateUrl: 'app/createCharacter/raceClassPicker/raceClassPicker.view.html'
-          },
-          'selection@createCharacter.raceClassPicker': {
-            templateUrl: 'app/createCharacter/raceClassPicker/picker.view.html'
-          },
-          'description@createCharacter.raceClassPicker': {
-            templateUrl: 'app/createCharacter/raceClassPicker/description.view.html'
-          }
-        }
+        controller: 'CreateCharacterCtrl as CreateCharacterCtrl',
+        templateUrl: 'app/createCharacter/raceClassPicker/raceClassPicker.view.html'
       });
   });

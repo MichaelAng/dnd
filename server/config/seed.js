9207abe9daa6399a366d192056a3b481e18b76ca
Michael Ang <angmerica@gmail.com>
2015-08-03 19:10:14 -0400

    add seed and routes for characterClass and race

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,6 +6,8 @@
 'use strict';
 
 var User = require('../api/user/user.model');
+var Races = require('../api/races/races.model');
+var CharacterClasses = require('../api/characterClasses/characterClasses.model');
 
 User.find({}).remove(function() {
   User.create({
@@ -21,6 +23,44 @@ User.find({}).remove(function() {
     password: 'admin'
   }, function() {
       console.log('finished populating users');
+    }
+  );
+});
+
+Races.find({}).remove(function() {
+  Races.create({
+    race: 'Human',
+    info: 'I am puny'
+  }, {
+    race: 'Halfling',
+    info: 'I am small'
+  }, {
+    race: 'Dwarf',
+    info: 'I am stocky'
+  }, {
+    race: 'Elf',
+    info: 'I am skinny'
+  }, function() {
+      console.log('finished populating races');
+    }
+  );
+});
+
+CharacterClasses.find({}).remove(function() {
+  CharacterClasses.create({
+    characterClass: 'Cleric',
+    info: 'I heal things'
+  }, {
+    characterClass: 'Fighter',
+    info: 'I bash things'
+  }, {
+    characterClass: 'Rogue',
+    info: 'I stab things'
+  }, {
+    characterClass: 'Wizard',
+    info: 'I blow things...up'
+  }, function() {
+      console.log('finished populating characterclasses');
     }
   );
 });

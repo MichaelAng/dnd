00bea12d0ace15b1876097b6384a841c1eb04d26
Michael Ang <angmerica@gmail.com>
2015-08-03 09:36:01 -0400

    fix code format

--- client/app/createCharacter/createCharacter.controller.js
+++ client/app/createCharacter/createCharacter.controller.js
@@ -7,15 +7,15 @@ angular.module('dndApp')
     vm.characterName = '';
 
     vm.races = [
-        {id: 1, race: 'Human',  description: 'a normal guy'},
-        {id: 2, race: 'Elf',  description: 'pointy ears'},
-        {id: 3, race: 'Dwarf',  description: 'Roughy and tough'}
+      {id: 1, race: 'Human',  description: 'a normal guy'},
+      {id: 2, race: 'Elf',  description: 'pointy ears'},
+      {id: 3, race: 'Dwarf',  description: 'Roughy and tough'}
     ];
 
     vm.characterClasses = [
-        {id: 1, characterClass: 'Cleric',  description: 'a normal guy'},
-        {id: 2, characterClass: 'Rogue',  description: 'pointy ears'},
-        {id: 3, characterClass: 'Wizard',  description: 'Roughy and tough'}
+      {id: 1, characterClass: 'Cleric',  description: 'a normal guy'},
+      {id: 2, characterClass: 'Rogue',  description: 'pointy ears'},
+      {id: 3, characterClass: 'Wizard',  description: 'Roughy and tough'}
     ];
 
   });

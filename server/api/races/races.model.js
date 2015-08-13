54dad08d2740b7e6ddc27eea377393631dc3b570
Michael Ang <angmerica@gmail.com>
2015-08-12 20:48:14 -0400

    add race Validation

--- server/api/races/races.model.js
+++ server/api/races/races.model.js
@@ -4,8 +4,90 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var RacesSchema = new Schema({
-  race: String,
-  info: String
+  race: {
+    type: String,
+    required: true
+  },
+  info: {
+    type: String,
+    required: true
+  },
+  abilityScoreIncrease: new Schema({
+    Strength: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    Dexterity: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    Constitution: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    Intelligence: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    Wisdom: {
+      type: Number,
+      default: 0,
+      required: true
+    },
+    Charisma: {
+      type: Number,
+      default: 0,
+      required: true
+    }
+  }, {
+    _id: false
+  }),
+  size: {
+    type: String,
+    enum: ['Small', 'Medium', 'Large'],
+    required: true,
+    default: 'Medium'
+  },
+  walkSpeed: {
+    type: Number,
+    required: true,
+    min: 0
+    default: 30
+  },
+  flightSpeed: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  swimSpeed: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  darkvision: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  languages: [{
+    type: String
+  }]
+});
+
+RacesSchema.pre('save', function (next) {
+
+  function hasCommonLanguage(element) {
+    return element === 'Common';
+  }
+
+  if ( !languages.some(hasCommonLanguage) ) {
+    languages.push('Common');
+  };
+  next();
 });
 
 module.exports = mongoose.model('Races', RacesSchema);

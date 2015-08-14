1af635a1c3d9541e1136c8491f3fadb8a4480b5c
Michael Ang <angmerica@gmail.com>
2015-08-14 19:02:10 -0400

    created a user

--- server/api/races/races.spec.js
+++ server/api/races/races.spec.js
@@ -6,9 +6,14 @@ var request = require('supertest');
 
 describe('GET /api/races', function() {
 
-  xit('should respond with JSON array', function(done) {
+  var auth = {};
+  before(createUserAndLogin(auth));
+
+  it('should respond with JSON array', function(done) {
+
     request(app)
       .get('/api/races')
+      .set('Authorization', 'bearer ' + auth.token)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
@@ -18,3 +23,24 @@ describe('GET /api/races', function() {
       });
   });
 });
+
+function createUserAndLogin(auth) {
+    return function(done) {
+        request(app)
+            .post('/api/users')
+            .send({
+                email: 'test@test.com',
+                password: 'test'
+            })
+            .expect(200)
+            .end(onResponse);
+
+        function onResponse(err, res) {
+            auth.token = res.body.token;
+            console.log('========================================');
+            console.log(auth.token);
+            console.log('========================================');
+            return done();
+        }
+    };
+}

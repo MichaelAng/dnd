f8d223de1644d1e4bf8b104be21d34ba6c73646d
Chris Sakai <cs.chris.sakai@gmail.com>
2015-08-17 22:10:50 -0400

    Update lodash. Also, add test to races.model.spec for default values.

--- server/api/races/races.model.spec.js
+++ server/api/races/races.model.spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var should = require('should');
 var app = require('../../app');
 var Race = require('./races.model');
@@ -9,6 +10,22 @@ var race = new Race({
   info : 'strong and short'
 });
 
+var raceDefaults = {
+  abilityScoreIncrease: {
+    strength: 0,
+    dexterity: 0,
+    constitution: 0,
+    intelligence: 0,
+    wisdom: 0,
+    charisma: 0
+  },
+  size: 'medium',
+  walkSpeed: 30,
+  flightSpeed: 0,
+  swimSpeed: 0,
+  darkvision: false
+};
+
 describe('Race Model', function() {
   before(function (done){
     //Clear races before testing
@@ -17,11 +34,12 @@ describe('Race Model', function() {
     });
   });
 
-  afterEach(function (done) {
-    Race.remove().exec().then(function() {
-      done();
-    });
-  });
+
+  // afterEach(function (done) {
+  //   Race.remove().exec().then(function() {
+  //     done();
+  //   });
+  // });
 
   it('should begin with no races', function (done) {
     Race.find({}, function (err, races) {
@@ -35,6 +53,16 @@ describe('Race Model', function() {
       Race.findOne({}, function (err, race) {
         race.should.have.property('race', 'Dwarf');
         race.should.have.property('info', 'strong and short');
+        done();
+      });
+    });
+  });
+
+  it('should save with race defaults.', function (done) {
+    race.save(function (err) {
+      Race.findOne({}, function (err, returnedRace) {
+        var _race = _.omit(returnedRace, ['race', 'info', 'languages']);
+        _.matches(raceDefaults)(_race).should.be.true;
         done();
       });
     });

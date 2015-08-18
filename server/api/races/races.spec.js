43f813c6466270ab7df9f3f714a7567f2fa88060
Michael Ang <angmerica@gmail.com>
2015-08-17 21:24:34 -0400

    create a bunch of tools for testing

--- server/api/races/races.spec.js
+++ server/api/races/races.spec.js
@@ -3,20 +3,21 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+
+var User = require('../user/user.model');
 var log = require('../../components/testingTools/logInOut');
 
 describe('GET /api/races', function() {
   var auth = {};
 
-  // Setup
+  // Setup Authorized User
   before(log.clearUsersCollection());
-  before(log.createUserAndlogin(auth));
+  before(log.createUserAndLogin(auth, 'test@test.com', 'test', 'admin'));
 
-  // Teardown
+  // Teardown User
   after(log.clearUsersCollection());
 
   it('should respond with JSON array', function(done) {
-
     request(app)
       .get('/api/races')
       .set('Authorization', 'bearer ' + auth.token)
--- server/components/testingTools/logInOut.js
+++ server/components/testingTools/logInOut.js
@@ -4,8 +4,14 @@ var app = require('../../app');
 var request = require('supertest');
 var User = require('../../api/user/user.model');
 
-exports.clearUsersCollection = function () {
-  return function(done) {
+exports.clearUsersCollection = clearUsersCollection;
+exports.createUser = createUser;
+exports.createUserAndLogin = createUserAndLogin;
+exports.deleteUser = deleteUser;
+exports.login = login;
+
+function clearUsersCollection () {
+  return function (done) {
     // Clear users before testing
     User.remove().exec().then(function() {
       done();
@@ -13,13 +19,45 @@ exports.clearUsersCollection = function () {
   };
 };
 
-exports.createUserAndlogin = function (auth) {
-  return function(done) {
+function createUser (email, password, role) {
+  return function (done) {
+    var user = new User({
+      provider: 'local',
+      name: 'Fake User',
+      email: email,
+      password: password,
+      role: role
+    });
+
+    user.save(function() {
+      done();
+    });
+  };
+};
+
+function createUserAndLogin (auth, email, password, role) {
+  return function (done) {
+    createUser(email, password, role)(function () {
+      login(auth, email, password)(done);
+    });
+  };
+};
+
+function deleteUser (email) {
+  return function (done) {
+    User.remove({ email: email }).exec().then(function() {
+      done();
+    });
+  };
+};
+
+function login (auth, email, password) {
+  return function (done) {
     request(app)
-      .post('/api/users')
+      .post('/auth/local')
       .send({
-        email: 'test@test.com',
-        password: 'test'
+        email: email,
+        password: password
       })
       .expect(200)
       .end(onResponse);
